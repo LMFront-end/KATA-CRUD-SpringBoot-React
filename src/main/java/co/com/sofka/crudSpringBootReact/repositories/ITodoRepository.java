90b2907137adfe0fb164ee333b2a6adf7adb1881
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-22 10:56:45 -0500

    se agregan metodos en el TodoServices

--- src/main/java/co/com/sofka/crudSpringBootReact/repositories/ITodoRepository.java
+++ src/main/java/co/com/sofka/crudSpringBootReact/repositories/ITodoRepository.java
@@ -3,6 +3,10 @@ package co.com.sofka.crudSpringBootReact.repositories;
 import co.com.sofka.crudSpringBootReact.models.Todo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ITodoRepository extends CrudRepository<Todo, Long> {
+
+
 
 }
--- src/main/java/co/com/sofka/crudSpringBootReact/services/TodoService.java
+++ src/main/java/co/com/sofka/crudSpringBootReact/services/TodoService.java
@@ -5,6 +5,8 @@ import co.com.sofka.crudSpringBootReact.repositories.ITodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TodoService {
 
@@ -16,5 +18,27 @@ public class TodoService {
         return itodoRepository.findAll();
     }
 
-    
+    // bucar por id
+    public Optional<Todo> findTById(Long id){
+        return itodoRepository.findById(id);
+    }
+
+    // crear una nueva tarea
+    public Todo save(Todo todo){
+        return itodoRepository.save(todo);
+    }
+
+    // eliminar una tarea
+    public void deleteTask(Long id){
+        itodoRepository.delete();
+    }
+
+    // obtener todo
+    public Todo get(Long id){
+        // si el id no existe nos muestra una exception
+        return itodoRepository.findById(id).orElseThrow();
+    }
+
+
+
 }
